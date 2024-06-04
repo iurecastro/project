ebd999e4207aec4bc87acad3630fba3c3efbabf0
iurecastro <55067724+iurecastro@users.noreply.github.com>
2024-06-04 23:42:15 +0100

    Get categories from model using Eloquent
    Get categories from model using Eloquent

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,13 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\DB;
+
+use App\Models\Category;
 class HomeController extends Controller
 {
     //
     public function index(){
         $allCategories = ["Category 1", "Category 2"];
 
-        $allCategories = DB::table('categories')->get();
+        // Get categories from database using facedes
+        //$allCategories = DB::table('categories')->get();
+
+        // Get categories from model using Eloquent (Laravel layer)
+        $allCategories = Category::all();
+
         //return view('home');
         return view('home', ['categories'=> $allCategories]);
 
