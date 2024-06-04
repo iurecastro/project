178a2f7b6158086ef251f5400e440e3f3c7d6086
iurecastro <55067724+iurecastro@users.noreply.github.com>
2024-06-04 22:21:29 +0100

    Create Second blade
    Create Second blade and route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/second', function () { 
+    return view('second');
+});
+
+//short version
+//Route::view('/second', 'second'); 
